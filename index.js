047cf7acbf167a2711cb53c85782bc17d9377bce
Kevin <kevincastillo333@gmail.com>
2023-12-01 03:11:28 -0500

    Add solution for lab

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+  // we'll fill this in shortly
+}
+
+function subtract() {
+  // we'll fill this in shortly
+}
+
+function multiply() {
+  // we'll fill this in shortly
+}
+
+function divide() {
+  // we'll fill this in shortly
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return ++n;
+}
+
+function decrement(n) {
+  return --n;
+}
+
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
